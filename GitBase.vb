0dcea1c61e8e5f4fc9b01daaa74b201596d52a78
Robert John Millano <bemanj87@gmail.com>
2018-01-10 19:25:55 +0800

    initial files

---
+++ GitBase.vb
@@ -0,0 +1,13 @@
+﻿Public Class GitBase
+    Private _last As String
+    Public Property LastName() As String
+        Get
+            Return _last
+        End Get
+        Set(ByVal value As String)
+            _last = value
+        End Set
+    End Property
+
+
+End Class
